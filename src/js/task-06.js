b594066a5aebe2b717cbe21c7ca5688fb7dedf60
Khayetska <113913429+Khayetska@users.noreply.github.com>
2023-03-19 00:56:42 +0200

    did  sixth task

---
+++ src/js/task-06.js
@@ -0,0 +1,17 @@
+const inputEl = document.querySelector("#validation-input");
+
+inputEl.addEventListener("blur", validationAmountOfSymbols);
+
+const dataLength = Number(inputEl.getAttribute("data-length"));
+
+function validationAmountOfSymbols(event) {
+  if (event.target.value.length === dataLength) {
+    event.target.classList.add("valid");
+    event.target.classList.remove("invalid");
+  } else {
+    event.target.classList.add("invalid");
+  }
+  if (event.target.value.length === 0) {
+    event.target.classList.remove("invalid", "valid");
+  }
+}
