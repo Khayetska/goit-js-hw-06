1fb193b18623f71eb4bc554620de89ac01831c21
Khayetska <113913429+Khayetska@users.noreply.github.com>
2023-03-19 23:39:38 +0200

    Did  eighth task

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,22 @@
+const loginFormEl = document.querySelector(".login-form");
+const submitBtnEl = document.querySelector("button");
 
+const emailInputEl = loginFormEl.elements.email;
+const passwordInputEl = loginFormEl.elements.password;
+
+loginFormEl.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  if (emailInputEl.value === "" || passwordInputEl.value === "") {
+    return window.alert("Всі поля повинні бути заповнені!");
+  }
+
+  const allData = {
+    [emailInputEl.name]: emailInputEl.value,
+    [passwordInputEl.name]: passwordInputEl.value,
+  };
+
+  console.log(allData);
+
+  event.target.reset();
+});
