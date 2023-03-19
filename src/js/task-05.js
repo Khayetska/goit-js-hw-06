4af2c399923ebfd7fc80e507d7e0b1edcd13174a
Khayetska <113913429+Khayetska@users.noreply.github.com>
2023-03-20 00:47:20 +0200

    Update task-05.js

--- src/js/task-05.js
+++ src/js/task-05.js
@@ -1,9 +1,9 @@
 const inputEl = document.querySelector("#name-input");
 const grettingEl = document.querySelector("#name-output");
 
-input.addEventListener("input", (event) => {
-  gretting.textContent = event.currentTarget.value;
-  if (gretting.textContent === "" || input.value[0] === " ") {
-    gretting.textContent = "Anonymous";
+inputEl.addEventListener("input", (event) => {
+  grettingEl.textContent = event.currentTarget.value;
+  if (grettingEl.textContent === "" || inputEl.value[0] === " ") {
+    grettingEl.textContent = "Anonymous";
   }
 });
